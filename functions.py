d291ce82475b40ffde80d6d5171c0def3eb717ff
mariaadradosr <mariaadradosr@gmail.com>
2020-01-22 13:16:22 +0100

    Bajas update

--- functions.py
+++ functions.py
@@ -116,6 +116,9 @@ def altas_cif(altas,tipo):
 def rfb(row):
     return row['rfb_migration'] if row['migrado'] == '1' else row['rfb_date']
 
+def baja__c(row):
+    return 0 if row['deactivation_date'] is pd.NaT else -1
+
 def bajas(cur):
     cur.execute('''
             select segmento, assetid, product_name, migrado,rfb_date, rfb_migration, deactivation_date, canal_venta, cif
@@ -137,4 +140,46 @@ def bajas(cur):
     df['Velocity'] = df.product_name.apply(lambda x: getVelocityFrom(x))
     df['isNeba'] = df.product_name.apply(lambda x: isNeba(x))
     df['Total'] = 1
+    df['Bajas__c'] = df.apply(lambda row: baja__c(row), axis = 1)
+    df['assets_act'] = df['Total'] + df['Bajas__c']
     return df
+
+def deactivation(row):
+    return pd.NaT if row['isActive'] >= 1 else row['deactivation_date']
+
+def altas_bajas(df):
+    assets = df.Total.groupby(by=[df.cif,df.canal_venta]).sum().reset_index()
+    assets_act = df.assets_act.groupby(by=[df.cif,df.canal_venta]).sum().reset_index()
+    altas = df.rfb__c.groupby(by=[df.cif,df.canal_venta]).min().reset_index()
+    bajas = df.deactivation_date.groupby(by=[df.cif,df.canal_venta]).max().reset_index()
+    final = pd.merge(pd.merge(pd.merge(assets_act,altas,on=['cif','canal_venta']),bajas,on=['cif','canal_venta']),assets,on=['cif','canal_venta'])
+    final['isActive'] = final.assets_act.apply(lambda x: 1 if x>=1 else 0)
+    final['deactivation_date'] = final.apply(lambda row: deactivation(row), axis = 1)
+    final['assets_dea'] = final['Total'] - final['assets_act']
+    final['isBaja'] = -(final['isActive']-1)
+    final['Client'] = 1
+    final.rename(columns={'Total':'assets_tot',
+                      'rfb__c':'rfb',
+                      }, inplace = True)
+    return final
+
+def altas_bajas_mes(altas_bajas_df):
+    altas = altas_bajas_df[['Client']].groupby(by=[altas_bajas_df.rfb]).sum()
+    bajas = altas_bajas_df[['isBaja']].groupby(by=[altas_bajas_df.deactivation_date]).sum()
+    bajas.index.name = 'rfb'
+    altas_bajas = altas.join(bajas, how='outer').fillna(0.0)
+    mes = altas_bajas.groupby(by=[altas_bajas.index.year, altas_bajas.index.month]).sum()
+    mes.index.names = ['year', 'month']
+    mes.reset_index(inplace=True)
+    return mes
+
+def altas_bajas_mes_canal(altas_bajas_df):
+    altas = altas_bajas_df[['Client']].groupby(by=[altas_bajas_df.rfb, altas_bajas_df.canal_venta]).sum()
+    bajas = altas_bajas_df[['isBaja']].groupby(by=[altas_bajas_df.deactivation_date,altas_bajas_df.canal_venta]).sum()
+    bajas.index.names = ['rfb','canal_venta']
+    altas_bajas = altas.join(bajas, how='outer').fillna(0.0)
+    mes_canal = altas_bajas.groupby(by=[altas_bajas.index.get_level_values('rfb').year, altas_bajas.index.get_level_values('rfb').month, altas_bajas.index.get_level_values('canal_venta')]).sum()
+    mes_canal.index.names = ['year', 'month', 'canal']
+    mes_canal.reset_index(inplace=True)
+    return mes_canal
+
