ec59386b965aa8361ef1b564ac7e1f62fb52e8f0
mariaadradosr <mariaadradosr@gmail.com>
2020-01-23 20:19:45 +0100

    funciones detalle, base, ventas added

--- functions.py
+++ functions.py
@@ -63,6 +63,35 @@ def getProductName(row):
         return 'Assets'
 
 
+def canal(canal_venta):
+    if re.search('DHO', canal_venta.upper()):
+        return 'DHO'
+    elif re.search('INTEGRA', canal_venta.upper()):
+        return 'IT integrator'
+    elif re.search('WEB', canal_venta.upper()):
+        return 'WEB'
+
+def rfb(row):
+    return row['rfb_migration'] if row['migrado'] == '1' else row['rfb_date']
+
+def purchase(row):
+    return row['purchase_migration'] if row['migrado'] == '1' else row['purchase_date']
+
+def purchaseToAsset(row):
+    return 0 if row['rfb__c'] is pd.NaT else 1
+
+def baja__c(row):
+    return 1 if row['deactivation_date'] is not pd.NaT else 0
+
+def cancelledSales(row):
+    if row['cancellation_date'] is pd.NaT and row['rfb__c'] != pd.NaT:
+        return 0
+    else:
+        return 1
+
+def deactivation(row):
+    return pd.NaT if row['isActive'] >= 1 else row['deactivation_date']
+
 def ventas(cur):
     cur.execute('''
                 select segmento, assetid, product_name, purchase_date, canal_venta, cif
@@ -131,12 +160,6 @@ def altas_cif(altas,tipo):
         df = df.groupby(by=['alta_year','alta_month']).count().reset_index()
         return df
 
-def rfb(row):
-    return row['rfb_migration'] if row['migrado'] == '1' else row['rfb_date']
-
-def baja__c(row):
-    return 0 if row['deactivation_date'] is pd.NaT else -1
-
 def bajas(cur):
     cur.execute('''
             select segmento, assetid, product_name, migrado,rfb_date, rfb_migration, deactivation_date, canal_venta, cif
@@ -161,9 +184,6 @@ def bajas(cur):
     df['Bajas__c'] = df.apply(lambda row: baja__c(row), axis = 1)
     df['assets_act'] = df['Total'] + df['Bajas__c']
     return df
-
-def deactivation(row):
-    return pd.NaT if row['isActive'] >= 1 else row['deactivation_date']
 
 def altas_bajas(df):
     assets = df.Total.groupby(by=[df.cif,df.canal_venta]).sum().reset_index()
@@ -241,3 +261,124 @@ def ventas_2(cur):
     df.drop(to_drop , inplace=True)
     df.drop(columns=['deactivation_request_date'], axis=1, inplace=True)
     return df
+
+
+
+# -------------------------------------------------------------------
+
+def detalle(cur):
+    cur.execute('''
+            select segmento, product_name, companyname, sitename, created_as_trial, purchase_date,
+            cancellation_date, service_date, rfb_date, deactivation_date, canal_venta, partnername,
+            price, cif, assetid, asset_status, migrado, assetpadre_mig, assethijo_mig, rfb_migration,
+            date_migration, purchase_migration
+            from cartera_xbo
+            where process_date = (
+            select max(process_date) from cartera_xbo)
+            and upper(product_name) not like '%CENTRALITA%'
+            ''')
+    df = pd.DataFrame(cur.fetchall())
+    col_names = []
+    for e in range(len(cur.description)):
+        col_names.append(cur.description[e][0])
+    df.rename(columns = dict(zip(list(range(33)), col_names)), inplace = True)
+    df['Family'] = df.product_name.apply(lambda x: getFamilyFrom(x))
+    df['Velocity'] = df.product_name.apply(lambda x: getVelocityFrom(x))
+    df['isNeba'] = df.product_name.apply(lambda x: isNeba(x))
+    df['product_name_ok'] = df.apply(lambda row: getProductName(row), axis = 1)
+    df['cod_postal'] = ''
+    df['precio_dto'] = ''
+    df['cod_postal'] = ''
+    df['%_dto'] = ''
+    df['Segmento_Empleados'] = ''
+    cols = ['segmento','Family', 'product_name', 'companyname', 'sitename', 'cod_postal',
+        'created_as_trial', 'purchase_date', 'cancellation_date', 
+        'service_date', 'rfb_date', 'deactivation_date', 'canal_venta', 
+        'partnername', 'price','precio_dto', '%_dto', 'cif', 'Segmento_Empleados','assetid', 'asset_status', 'migrado', 
+        'assetpadre_mig', 'assethijo_mig', 'rfb_migration', 'date_migration', 'purchase_migration','product_name_ok']
+    df = df[cols]
+    return df
+
+
+def base(cur):
+    cur.execute('''
+            select segmento, assetid, cif, canal_venta, product_name, 
+                    migrado,rfb_date, rfb_migration, deactivation_date, cancellation_date,
+                    purchase_date, purchase_migration, deactivation_request_date
+            from cartera_xbo
+            where process_date = (
+            select max(process_date) from cartera_xbo)
+            and segmento in ('3-NOTRIAL')
+            and upper(product_name) not like '%CENTRALITA%'
+            ''')
+    df = pd.DataFrame(cur.fetchall())
+    col_names = []
+    for e in range(len(cur.description)):
+        col_names.append(cur.description[e][0])
+    df.rename(columns = dict(zip(list(range(33)), col_names)), inplace = True)
+    df['canal_venta'] = df.canal_venta.apply(lambda x: canal(x))
+    df['rfb__c'] = df.apply(lambda row: rfb(row), axis = 1)
+    df.drop(columns=['rfb_date','rfb_migration'], axis=1, inplace=True)
+    df['purchase_date__c'] = df.apply(lambda row: purchase(row), axis = 1)
+    df.drop(columns=['purchase_migration','purchase_date'], axis=1, inplace=True)
+    df['Family'] = df.product_name.apply(lambda x: getFamilyFrom(x))
+    df['Velocity'] = df.product_name.apply(lambda x: getVelocityFrom(x))
+    df['isNeba'] = df.product_name.apply(lambda x: isNeba(x))
+    df['product_name'] = df.apply(lambda row: getProductName(row), axis = 1)
+    df['isSold'] = 1
+    df['isAsset'] =  df.apply(lambda row: purchaseToAsset(row), axis = 1)
+    df['isBaja'] = df.apply(lambda row: baja__c(row), axis = 1)
+    df['isActive'] = df['isAsset'] - df['isBaja']
+    df['isCancelled'] = df.apply(lambda row: cancelledSales(row), axis = 1)
+    to_drop = df[(df['rfb__c'].isnull())&((df['deactivation_date'].notnull())|(df['deactivation_request_date'].notnull()))].index
+    df.drop(to_drop , inplace=True)
+    df.drop(columns=['deactivation_request_date'], axis=1, inplace=True)
+    return df
+
+# -------------- VENTAS --------------
+
+# VENTA ASSETS
+def ass_vta_fam_mes(df,canal=0):
+    if canal == 1:
+        ass_vta_canal_fam_mes = df[['isSold']].groupby(by=[df.purchase_date__c.dt.year,df.purchase_date__c.dt.month,df.canal_venta,df.Family]).sum()
+        ass_vta_canal_fam_mes.index.names = ['purchase_year', 'purchase_month', 'canal_venta','Family']
+        ass_vta_canal_fam_mes.reset_index(inplace=True)
+        return ass_vta_canal_fam_mes.pivot_table('isSold',['canal_venta','Family'],['purchase_year','purchase_month'],fill_value=0,margins=True,aggfunc=sum)
+    elif canal == 0:
+        ass_vta_fam_mes = df[['isSold']].groupby(by=[df.purchase_date__c.dt.year,df.purchase_date__c.dt.month,df.Family]).sum()
+        ass_vta_fam_mes.index.names = ['purchase_year', 'purchase_month','Family']
+        ass_vta_fam_mes.reset_index(inplace=True)
+        return ass_vta_fam_mes.pivot_table('isSold','Family',['purchase_year','purchase_month'],fill_value=0,margins=True,aggfunc=sum)
+    else:
+        print('Canal: 0 or 1')
+
+def ass_vta_prod_mes(df,canal=0):
+    if canal == 1:
+        ass_vta_canal_prod_mes = df[['isSold']].groupby(by=[df.purchase_date__c.dt.year,df.purchase_date__c.dt.month,df.canal_venta,df.Family,df.product_name]).sum()
+        ass_vta_canal_prod_mes.index.names = ['purchase_year', 'purchase_month','canal_venta','Family','product_name']
+        ass_vta_canal_prod_mes.reset_index(inplace=True)
+        return ass_vta_canal_prod_mes.pivot_table('isSold',['canal_venta','Family','product_name'],['purchase_year','purchase_month'],fill_value=0,margins=True,aggfunc=sum)
+    elif canal == 0:
+        ass_vta_canal_prod_mes = df[['isSold']].groupby(by=[df.purchase_date__c.dt.year,df.purchase_date__c.dt.month,df.Family,df.product_name]).sum()
+        ass_vta_canal_prod_mes.index.names = ['purchase_year', 'purchase_month','Family','product_name']
+        ass_vta_canal_prod_mes.reset_index(inplace=True)
+        return ass_vta_canal_prod_mes.pivot_table('isSold',['Family','product_name'],['purchase_year','purchase_month'],fill_value=0,margins=True,aggfunc=sum)
+    else:
+        print('Canal: 0 or 1')
+        
+# VENTAS CLIENTES
+
+
+def cli_vta_fam_mes(df,canal=0):
+    if canal == 0:
+        cli_vta_canal_mes = df[['cif']].groupby(by=[df.purchase_date__c.dt.year,df.purchase_date__c.dt.month,df.canal_venta]).nunique()
+        cli_vta_canal_mes.index.names = ['purchase_year', 'purchase_month', 'canal_venta']
+        cli_vta_canal_mes.reset_index(inplace=True)
+        return cli_vta_canal_mes.pivot_table('cif',['canal_venta'],['purchase_year','purchase_month'],fill_value=0,margins=True,aggfunc=sum)
+    elif canal == 1:
+        cli_vta_canal_fam_mes = df[['cif']].groupby(by=[df.purchase_date__c.dt.year,df.purchase_date__c.dt.month,df.canal_venta,df.Family]).nunique()
+        cli_vta_canal_fam_mes.index.names = ['purchase_year', 'purchase_month', 'canal_venta','Family']
+        cli_vta_canal_fam_mes.reset_index(inplace=True)
+        return cli_vta_canal_fam_mes.pivot_table('cif',['canal_venta','Family'],['purchase_year','purchase_month'],fill_value=0)
+    else:
+        print('Canal: 0 or 1')
